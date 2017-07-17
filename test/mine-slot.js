8f396b4889e3de50c8dae22722427eab3a198b6e
Alberto Romero Navia <lguitarras0594@gmail.com>
2017-07-17 10:55:34 -0500

    Add more tests for mine

--- test/mine-slot.js
+++ test/mine-slot.js
@@ -2,6 +2,11 @@ import MineSlot from '../minesweeper/slots/mine';
 
 describe('Mine Slot', () => {
   const buildMineSlot = () => new MineSlot();
+  it('should be created without a flag', () => {
+    const mine = buildMineSlot();
+    expect(mine.hasFlag()).to.equals(false);
+  });
+
   it('should not have a flag', () => {
     const mine = buildMineSlot();
     expect(mine.hasFlag()).to.equals(false);
@@ -11,5 +16,12 @@ describe('Mine Slot', () => {
     const mine = buildMineSlot();
     mine.flag();
     expect(mine.hasFlag()).to.equals(true);
+  });
+
+  it('should be able to unset a flag', () => {
+    const mine = buildMineSlot();
+    mine.flag();
+    mine.unflag();
+    expect(mine.hasFlag()).to.equals(false);
   });
 });
